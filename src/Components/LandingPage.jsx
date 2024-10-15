2a4c5dcc16f598580ec421a8f06d5d8c6de9b142
Amandeep <145090954+amandeep-boot@users.noreply.github.com>
2024-10-15 12:43:13 +0530

    landing page

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,12 +1,21 @@
 import LandingImg from "../Images/Landingimg1.jpeg";
+import styles from "./LandingPage.module.css"
+import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
-    <>
-      {/* <img src={LandingImg} className="LPimg" /> */}
+    <div className={styles.landing}>
+      <img src={LandingImg} className="LPimg" />
       <h1 className="LPHeading">UNCOVERING FOOD FACTS</h1>
       <h4>Fact Check Your Food : A simple Guide To Brand Claims</h4>
-    </>
+      <Link className={`${styles.button} mb-5`} to="/ClaimCheckOptions">Check Claims</Link>
+      <div className=" w-50 mx-auto mt-5 ">
+        <div className="fs-4 fw-bold">
+            OUR OBJECTIVE
+        </div>
+        <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus aperiam eveniet consequuntur, accusantium illum ut. Distinctio dolorem omnis impedit porro debitis quasi provident magnam hic, beatae facilis! Suscipit, nam?</div>
+      </div>
+    </div>
   );
 }
 export default LandingPage;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,16 @@
 // import logo from "../Images/Logo.png";
 import styles from "./NavBar.module.css";
 import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 function NavBar() {
+  const location = useLocation();
+
+  // Check if the current route is "/"
+  const isHome = location.pathname === "/";
   return (
-    <>
+    <div style={{
+      backgroundColor:isHome?'white':'transparent'
+    }}>
       <div className={styles.navbar}>
         <div className={styles.logo}>
           {/* <img src={logo} className={styles.logoImg}></img> */}
@@ -16,7 +23,7 @@ function NavBar() {
             <Link className={styles.links} to="/">Contact Us</Link>
         </div>
       </div>
-    </>
+    </div >
   );
 }
 export default NavBar;
