a2f9d433a3750be74933b7c8b6a8c22b8ffefee2
Amandeep <145090954+amandeep-boot@users.noreply.github.com>
2024-10-15 22:52:20 +0530

    landing feature

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,21 +1,71 @@
 import LandingImg from "../Images/Landingimg1.jpeg";
-import styles from "./LandingPage.module.css"
+import styles from "./LandingPage.module.css";
 import { Link } from "react-router-dom";
+import f1 from "../Images/locker-feature.png";
+import f2 from "../Images/significance-feature.png";
+import f3 from "../Images/sprout-feature.png";
+import FeatureCard from "./FeatureCard";
 
 function LandingPage() {
+  const features = [
+    {
+      img: f1,
+      title: "feature-1",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, maxime!",
+    },
+    {
+      img: f2,
+      title: "feature-1",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, maxime!",
+    },
+    {
+      img: f3,
+      title: "feature-1",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, maxime!",
+    },
+  ];
   return (
-    <div className={styles.landing}>
-      <img src={LandingImg} className="LPimg" />
-      <h1 className="LPHeading">UNCOVERING FOOD FACTS</h1>
-      <h4>Fact Check Your Food : A simple Guide To Brand Claims</h4>
-      <Link className={`${styles.button} mb-5`} to="/ClaimCheckOptions">Check Claims</Link>
-      <div className=" w-50 mx-auto mt-5 ">
-        <div className="fs-4 fw-bold">
-            OUR OBJECTIVE
-        </div>
-        <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus aperiam eveniet consequuntur, accusantium illum ut. Distinctio dolorem omnis impedit porro debitis quasi provident magnam hic, beatae facilis! Suscipit, nam?</div>
+    <>
+      <div className={styles.landing}>
+        <img src={LandingImg} className="LPimg" />
+        <h1 className="LPHeading">UNCOVERING FOOD FACTS</h1>
+        <h4>Fact Check Your Food : A simple Guide To Brand Claims</h4>
+        <Link className={`${styles.button}`} to="/ClaimCheckOptions">
+          Check Claims
+        </Link>
       </div>
-    </div>
+
+      <div className="bg-white">
+        <div className=" w-50 mx-auto ">
+          <div className="fs-3 fw-bold">OUR OBJECTIVE</div>
+          <h4>"Trust What You Eat – Verify with Confidence!"</h4>
+        </div>
+        <div className="bg-white mx-auto w-75 mb-5">
+          {" "}
+          Empowering consumers to make informed, confident choices by providing
+          an easy-to-use platform that verifies whether brand claims on edible
+          items align with their ingredients. By providing clear, unbiased
+          insights, we aim to help users make better-informed decisions about
+          the food they consume, fostering trust and accountability in the
+          marketplace. Our goal is to create a user-friendly space where
+          consumers can easily cut through marketing jargon and confidently
+          assess the authenticity of product claims
+        </div>
+
+        <div
+          className="w-100"
+          style={{ height: "75vh", backgroundColor: "rgb(128 0 128 / 13%)" }}
+        >
+          {" "}
+          <div className="fs-3 fw-bold pt-5">FEATURES</div>
+          <div className="d-flex justify-content-evenly mt-4 w-75 mx-auto"> 
+            {
+              features.map(feature=><FeatureCard val={feature}/> )
+            } 
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 export default LandingPage;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -14,7 +14,7 @@ function NavBar() {
       <div className={styles.navbar}>
         <div className={styles.logo}>
           <img src={logo} className={styles.logoImg}></img>
-          <div>PureClaim</div>
+          <div className="fw-bold">PureClaim</div>
         </div>
         <div className={styles.navs}>
             <Link  className={`${styles.links} ${styles.active} `} to="/">Home</Link>
