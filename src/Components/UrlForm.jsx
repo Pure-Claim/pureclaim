bd3524b5a05097572776640bda6f166cf2ff8f95
Sanjana Chauhan <146856565+Sanjana-Chauhan@users.noreply.github.com>
2024-10-21 22:16:12 +0530

    code optimized

--- src/Components/UrlForm.jsx
+++ src/Components/UrlForm.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
 import styles from "./UrlForm.module.css";
 import img from "../Images/swiggy1.png";
 import zepto from "../Images/Zepto.png";
@@ -7,65 +6,58 @@ import amazon from "../Images/amazon.png";
 import BigBasket from "../Images/BigBasket.png";
 import JioMart from "../Images/JioMart.png";
 import Blinkit from "../Images/Blinkit.png";
+import { ContextProvider } from "../Store/ContextProvider";
+import WebsiteCards from "./WebsiteCards";
 
 function UrlForm() {
-
+  const siteCards = [
+    {
+      siteName: "Swiggy-instamart",
+      imgPath: img,
+    },
+    {
+      siteName: "blinkit",
+      imgPath: Blinkit,
+    },
+    {
+      siteName: "zepto",
+      imgPath: zepto,
+    },
+    {
+      siteName: "BigBasket",
+      imgPath: BigBasket,
+    },
+    {
+      siteName: "Amazon",
+      imgPath: amazon,
+    },
+    {
+      siteName: "Jiomart",
+      imgPath: JioMart,
+    },
+  ];
   const [url, setUrl] = useState("");
   const [selectedWebsite, setSelectedWebsite] = useState("");
   const [error, setError] = useState(null);
   const [productData, setProductData] = useState(null);
   const [loading, setLoading] = useState(false);
-  const[Analyzing,setAnalyzing]=useState(false);
- const navigate=useNavigate();
-  function handleAnalyse() {
-    const Claims = productData.productTitle;
-    const Ingredients = productData.ingredientsText;
-   
-    if (Claims == "" || Ingredients == "") {
-      alert("All Fields are mandatory!");
-      return;
-    }
-    const encodedClaim = encodeURIComponent(Claims);
-    const encodedIngredients = encodeURIComponent(Ingredients);
-    const apiUrl = `https://cwbackend-a3332a655e1f.herokuapp.com/claims/analyze?claim=${encodedClaim}&ingredients=${encodedIngredients}`;
-    setAnalyzing(true);
-    fetch(apiUrl, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setAnalyzing(false);
-       
-        if (data) {
-          navigate("/ClaimCheckOptions/Manualform/Output", {
-            state: { data: data },
-          });
-        }
-      }).catch((error)=>{
-        console.log(error);
-        throw(error);
-      });
-    }
+  const { isAnalyzing, handleAnalyse } = useContext(ContextProvider);
 
-  const handleOptionChange=(e)=>{
+  const handleOptionChange = (e) => {
+    console.log(e.target.value);
     setSelectedWebsite(e.target.value);
-    console.log(e.target.value)
-  }
+  };
 
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    // Clear previous states and errors
+    e.preventDefault();  
     setError(null);
     setProductData(null);
-    setLoading(true); // Set loading to true when starting fetch
+    setLoading(true);
 
     if (!url || !selectedWebsite) {
       setError("Please enter a URL and select a website");
-      setLoading(false); // Set loading to false on error
+      setLoading(false);
       return;
     }
 
@@ -77,7 +69,6 @@ function UrlForm() {
       );
       const data = await response.json();
       if (response.ok) {
-        // Store the extracted data in state
         setProductData(data);
       } else {
         setError(data.error);
@@ -85,13 +76,12 @@ function UrlForm() {
     } catch (err) {
       setError("An error occurred while fetching data.");
     } finally {
-      setLoading(false); // Set loading to false after fetch is complete
+      setLoading(false);
     }
   };
 
   return (
     <div className={styles.URLContainer}>
-     
       <div className={styles.UrlForm}>
         <input
           type="url"
@@ -101,111 +91,33 @@ function UrlForm() {
           required
           className={styles.urlInput}
         />
-
+    {/* All site cards  */}
         <div className={styles.websiteList}>
-  
-          <label className={`${styles.WebsiteLabel}  ${selectedWebsite=="swiggy-instamart"? styles.selected  : "" }`}>
-          <div className={styles.labelimg}>
-              <img src={img}  />
-            </div>
-            <input
-              type="radio"
-              name="website"
-              value="swiggy-instamart"
-              checked={selectedWebsite === "swiggy-instamart"}
-              onChange={handleOptionChange}
-              className={styles.radioBtn}
+          {siteCards.map((site) => (
+            <WebsiteCards
+              siteName={site.siteName}
+              imgPath={site.imgPath}
+              handleOptionChange={handleOptionChange}
+              selectedWebsite={selectedWebsite}
             />
-            Swiggy Instamart
-          </label>
-
-          <label className={`${styles.WebsiteLabel}  ${selectedWebsite=="blinkit"? styles.selected  : "" }`}>
-          <div className={styles.labelimg}>
-              <img src={Blinkit}  />
-            </div>
-            <input
-              type="radio"
-              name="website"
-              value="blinkit"
-              checked={selectedWebsite === "blinkit"}
-              onChange={handleOptionChange}
-              className={styles.radioBtn}
-            />
-            Blinkit
-          </label>
-          <label className={`${styles.WebsiteLabel}  ${selectedWebsite=="zepto"? styles.selected  : "" }`}>
-            <div className={styles.labelimg}>
-              <img src={zepto}  />
-            </div>
-            <input
-              type="radio"
-              name="website"
-              value="zepto"
-              checked={selectedWebsite === "zepto"}
-              onChange={handleOptionChange}
-              className={styles.radioBtn}
-            />
-            <span> Zepto</span>
-          </label>
-
-
-          <label className={`${styles.WebsiteLabel}  ${selectedWebsite=="bigbasket"? styles.selected  : "" }`}>
-          <div className={styles.labelimg}>
-              <img src={BigBasket}  />
-            </div>
-            <input
-              type="radio"
-              name="website"
-              value="bigbasket"
-              checked={selectedWebsite === "bigbasket"}
-              onChange={handleOptionChange}
-              className={styles.radioBtn}
-            />
-            Big Basket
-          </label>
-
-          <label className={`${styles.WebsiteLabel}  ${selectedWebsite=="amazon"? styles.selected  : "" }`}>
-          <div className={styles.labelimg}>
-              <img src={amazon}  />
-            </div>
-            <input
-              type="radio"
-              name="website"
-              value="amazon"
-              checked={selectedWebsite === "amazon"}
-              onChange={handleOptionChange}
-              className={styles.radioBtn}
-            />
-            Amazon
-          </label>
-          <label className={`${styles.WebsiteLabel}  ${selectedWebsite=="jiomart"? styles.selected  : "" }`}>
-          <div className={styles.labelimg}>
-              <img src={JioMart}  />
-            </div>
-            <input
-              type="radio"
-              name="website"
-              value="jiomart"
-              checked={selectedWebsite === "jiomart"}
-              onChange={handleOptionChange}
-              className={styles.radioBtn}
-            />
-            Jio Mart
-          </label>
-
+          ))}
         </div>
-        {!productData && <button
-          type="submit"
-          disabled={loading}
-          onClick={handleSubmit}
-          className={styles.submitBtn}
-        >
-          Submit
-        </button>}
-        {/* Disable button during loading */}
+
+
+        {!productData && (
+          <button
+            type="submit"
+            disabled={loading}
+            onClick={handleSubmit}
+            className={styles.submitBtn}
+          >
+            Submit
+          </button>
+        )}
+       
 
         {loading && <p>Analyzing...</p>}
-        
+
         {error && <p style={{ color: "red" }}>{error}</p>}
         {productData && (
           <div style={{ marginTop: "20px" }}>
@@ -218,9 +130,18 @@ function UrlForm() {
             </p>
           </div>
         )}
-        {productData && 
-        <button className={styles.analyzebtn} onClick={handleAnalyse}>{ Analyzing ? "Analyzing..." : "Analyze"}</button>
-}
+        {productData && (
+          <button
+            className={styles.analyzebtn}
+            onClick={() => {
+              const Claims = productData.productTitle;
+              const Ingredients = productData.ingredientsText;
+              handleAnalyse(Claims, Ingredients);
+            }}
+          >
+            {isAnalyzing ? "Analyzing..." : "Analyze"}
+          </button>
+        )}
       </div>
     </div>
   );
